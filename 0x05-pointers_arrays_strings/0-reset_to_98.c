c10efd57cd2589ac832891ca86eb47e698ebab96
salimomondi2000 <salimomondi2000@gmail.com>
2023-03-27 10:41:28 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function declaration
- * @n: pointer integer
- * Return: void
- */
-
-void reset_to_98(int *n);
-
-/**
- * reset_to_98 - function definition
- * Description: resets value to 98
- * @n: the pointer integer
- * Return: void
+ * reset_to_98 - main
+ * @n: - pinter to int
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
