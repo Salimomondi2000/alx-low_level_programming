0f4d00a65332b745dd66883099734ce8250a984a
salimomondi2000 <salimomondi2000@gmail.com>
2023-04-11 00:40:58 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: integer value counting the number of arguments
+ * @argv: array of strings
+ * Return: it returns 0 if the program compiles ok
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
